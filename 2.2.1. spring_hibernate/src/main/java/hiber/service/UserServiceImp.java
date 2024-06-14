ef3db437a1b2e03cc8e631fccc5ce6c4902c0e10
йй <woolf_32@mail.ru>
2024-06-14 18:12:34 +0300

    Override getUserByCar

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,10 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional
+   @Override
+   public User getUserByCar(String model, int series) {
+      return userDao.getUserByCar(model,series);
+   }
+
 }
